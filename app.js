1ca15af3a51b59ad8fb6016ed1c9de79b30d9874
JesusManuelMallo <88166930+JesusManuelMallo@users.noreply.github.com>
2021-09-29 17:50:38 +0200

    prueba commit

--- app.js
+++ app.js
@@ -4,10 +4,10 @@ const express = require("express");
 
 const { isAuthenticated } = require("./middleware/jwt.middleware"); // <== IMPORT
 
-
+//
 const app = express();
 require("./config")(app);
-
+////////////
 
 // 👇 MIDDLEWARE MISSING
 const allRoutes = require("./routes");
@@ -21,7 +21,7 @@ app.use("/api", isAuthenticated, projectRouter);            // <== UPDATE
 
 const taskRouter = require("./routes/task.routes");
 app.use("/api",isAuthenticated, taskRouter);            // <== UPDATE
-//
+////
 /* 
 app.use((req, res, next) => {
     // If no routes match, send them the React HTML.
