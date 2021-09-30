6cf5cefe9ef1e32978cd97292f7dabeba354aa13
Jesus <jesus.mallo.paya@gmail.com>
2021-09-30 10:04:30 +0200

    2nd day backend commit

--- routes/song.routes.js
+++ routes/song.routes.js
@@ -2,70 +2,70 @@ const express = require("express");
 const router = express.Router();
 const mongoose = require("mongoose");
 
-const Project = require("../models/Project.model");
+const Song = require("../models/Song.model");
 const Task = require("../models/Task.model");
 
 //  POST /api/projects  -  Creates a new project
-router.post("/projects", (req, res, next) => {
+router.post("/songs", (req, res, next) => {
   const { title, description } = req.body;
 
-  Project.create({ title, description, tasks: [] })
+  Song.create({ title, description, tasks: [] })
     .then((response) => res.json(response))
     .catch((err) => res.json(err));
 });
 
 //  GET /api/projects -  Retrieves all of the projects
-router.get("/projects", (req, res, next) => {
-  Project.find()
+router.get("/songs", (req, res, next) => {
+  Song.find()
     .populate("tasks")
-    .then((allProjects) => res.json(allProjects))
+    .then((allSongs) => res.json(allSongs))
     .catch((err) => res.json(err));
 });
 
 //  GET /api/projects/:projectId -  Retrieves a specific project by id
-router.get("/projects/:projectId", (req, res, next) => {
-  const { projectId } = req.params;
+router.get("/songs/:songId", (req, res, next) => {
+  const { songId: songId } = req.params;
 
-  if (!mongoose.Types.ObjectId.isValid(projectId)) {
+  if (!mongoose.Types.ObjectId.isValid(songId)) {
     res.status(400).json({ message: "Specified id is not valid" });
     return;
   }
 
   // Each Project document has `tasks` array holding `_id`s of Task documents
   // We use .populate() method to get swap the `_id`s for the actual Task documents
-  Project.findById(projectId)
+  Song.findById(songId)
     .populate("tasks")
-    .then((project) => res.status(200).json(project))
+    .then((song) => res.status(200).json(song))
     .catch((error) => res.json(error));
 });
 
 // PUT  /api/projects/:projectId  -  Updates a specific project by id
-router.put("/projects/:projectId", (req, res, next) => {
-  const { projectId } = req.params;
+router.put("/songs/:songId", (req, res, next) => {
+  const { songId } = req.params;
 
   if (!mongoose.Types.ObjectId.isValid(projectId)) {
     res.status(400).json({ message: "Specified id is not valid" });
     return;
   }
 
-  Project.findByIdAndUpdate(projectId, req.body, { new: true })
-    .then((updatedProject) => res.json(updatedProject))
+  Song.findByIdAndUpdate(songId, req.body, { new: true })
+    .then((updatedSong) => res.json(updatedSong))
     .catch((error) => res.json(error));
 });
 
 // DELETE  /api/projects/:projectId  -  Deletes a specific project by id
-router.delete("/projects/:projectId", (req, res, next) => {
-  const { projectId } = req.params;
+router.delete("songs/:songId", (req, res, next) => {
+  const { songId } = req.params;
 
-  if (!mongoose.Types.ObjectId.isValid(projectId)) {
+  if (!mongoose.Types.ObjectId.isValid(songId)) {
     res.status(400).json({ message: "Specified id is not valid" });
     return;
   }
 
-  Project.findByIdAndRemove(projectId)
+  Song.findByIdAndRemove(songId)
     .then(() =>
       res.json({
-        message: `Project with ${projectId} is removed successfully.`,
+        message: `Song with ${songId} is removed successfully.`,
       })
     )
     .catch((error) => res.json(error));
